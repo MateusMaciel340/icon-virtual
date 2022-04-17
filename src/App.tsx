13978219a545b682049ea00c467d7318057b3138
Mateus Sousa <mateusmaciel340@gmail.com>
2022-04-16 23:20:12 -0300

    Inserção do react-router-dom | styled-components / estrutura das rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import Rotas from "./Router";
+import { GlobalStyle } from "./styles/GlobalStyle";
+
 function App() {
   return (
       <>
-        <h1>Hello World!</h1>
+        <Rotas/>
+        <GlobalStyle/>
       </>
   );
 }
