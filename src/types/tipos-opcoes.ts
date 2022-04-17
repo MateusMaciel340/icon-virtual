ec559f2fcdc2fe1fefc0bf634630465db319a60d
Mateus Sousa <mateusmaciel340@gmail.com>
2022-04-17 08:14:23 -0300

    Tipificação de clientes e categorias | Atualização da imagem logo

--- src/types/tipos-opcoes.ts
+++ src/types/tipos-opcoes.ts
@@ -1,3 +1,13 @@
 export type Opcoes = {
     id: number, tipo: string, titulo: string, imagem: string, link: string,
 }
+
+export type GrupoClientes = {
+    cpf_cliente: string, email_cliente: string, endereco_cliente: string,
+    id_cliente: number, nome_cliente: string, senha_cliente: string,
+    telefone_cliente: number,
+}
+
+export type GrupoCategorias = {
+    id_categoria: number, titulo_categoria: string, descricao_categoria: string,
+}
